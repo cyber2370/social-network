3ccab99b87868c8836dbd04a4812e8b1f60ace64
user <user@DESKTOP-FUU8Q9O>
2016-11-18 22:05:45 +0200

    Add reducers empty files

--- SocialNetwork/SocialNetwork/scripts/redux/reducers/index.js
+++ SocialNetwork/SocialNetwork/scripts/redux/reducers/index.js
@@ -1,7 +1,18 @@
 ﻿import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+import friends from './friends';
+import locations from './locations';
+import workplaces from './workplaces';
+import messages from './messages';
+import userWorkplaces from './userWorkplaces';
+
 const rootReducer = combineReducers({
+	friends,
+	locations,
+	workplaces,
+	messages,
+	userWorkplaces,
 	routing: routerReducer
 });
 
