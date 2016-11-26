4e323e8047394123307ae0ce958893a999f28f3d
cyber2370 <suppy2370@gmail.com>
2016-11-26 02:28:12 +0200

    Small changes in registerviewmodel

---
+++ SocialNetwork/SocialNetwork-UWP/Presentation/Models/RegistrationModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SocialNetwork_UWP.Presentation.Models
+{
+    class RegistrationModel
+    {
+
+        public class RegisterBindingModel
+        {
+            public string Email { get; set; }
+            
+            public string Password { get; set; }
+            
+            public string ConfirmPassword { get; set; }
+        }
+    }
+}
---
+++ SocialNetwork/SocialNetwork-UWP/Presentation/ViewModels/Login/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using SocialNetwork_UWP.Presentation.ViewModels.Common;
+
+namespace SocialNetwork_UWP.Presentation.ViewModels.Login
+{
+    public class RegisterViewModel : ViewModelBase
+    {
+        public RegisterViewModel()
+        {
+             RegisterCommand = new RelayCommand(Register);
+        }
+
+        public ICommand RegisterCommand { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        private void Register()
+        {
+            var registrationModel = new RegisterViewModel
+            {
+                Email = Email,
+                Password = Password,
+                ConfirmPassword = ConfirmPassword
+            };
+
+            // send model to server
+
+            // if success then navigate to profile creating
+        }
+
+    }
+}
